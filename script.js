e183303abf6cc23de864c18caaee2d891751282a
vmalabanan <63671843+vmalabanan@users.noreply.github.com>
2022-04-27 18:43:52 -0700

    update hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Make mobile navigation work
 const btnNavEl = document.querySelector(".button-mobile-nav");
 const headerEl = document.querySelector(".main-header");
+const navLinkEl = document.querySelector(".nav-list");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open"); // If 'nav-open' does not appear in the headerEl class, then add it. If not, remove.
+});
+
+navLinkEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
